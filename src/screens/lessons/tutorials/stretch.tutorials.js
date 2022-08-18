4acaab2d174ee015df29589ada36042c836522bb
Logan Cundiff <lcundiff@ufl.edu>
2022-08-18 14:56:07 -0400

    rename stretch imgs :truck:

--- src/screens/lessons/tutorials/stretch.tutorials.js
+++ src/screens/lessons/tutorials/stretch.tutorials.js
@@ -1,50 +1,50 @@
 
 export const section1 = [
-    require('../../../images/tutorials/stretches/1.png'),
-    require('../../../images/tutorials/stretches/2.png'),
-    require('../../../images/tutorials/stretches/3.png'),
-    require('../../../images/tutorials/stretches/4.png'),
-    require('../../../images/tutorials/stretches/5.png'),
-    require('../../../images/tutorials/stretches/6.png')
+    require('../../../images/tutorials/stretches/1.jpg'),
+    require('../../../images/tutorials/stretches/2.jpg'),
+    require('../../../images/tutorials/stretches/3.jpg'),
+    require('../../../images/tutorials/stretches/4.jpg'),
+    require('../../../images/tutorials/stretches/5.jpg'),
+    require('../../../images/tutorials/stretches/6.jpg')
 ];
 export const section2 = [
-    require('../../../images/tutorials/stretches/7.png'),
-    require('../../../images/tutorials/stretches/8.png'),
-    require('../../../images/tutorials/stretches/9.png'),
-    require('../../../images/tutorials/stretches/10.png'),
-    require('../../../images/tutorials/stretches/11.png'),
-    require('../../../images/tutorials/stretches/12.png'),
-    require('../../../images/tutorials/stretches/13.png'),
-    require('../../../images/tutorials/stretches/14.png'),
-    require('../../../images/tutorials/stretches/15.png'),
-    require('../../../images/tutorials/stretches/16.png'),
-    require('../../../images/tutorials/stretches/17.png')
+    require('../../../images/tutorials/stretches/7.jpg'),
+    require('../../../images/tutorials/stretches/8.jpg'),
+    require('../../../images/tutorials/stretches/9.jpg'),
+    require('../../../images/tutorials/stretches/10.jpg'),
+    require('../../../images/tutorials/stretches/11.jpg'),
+    require('../../../images/tutorials/stretches/12.jpg'),
+    require('../../../images/tutorials/stretches/13.jpg'),
+    require('../../../images/tutorials/stretches/14.jpg'),
+    require('../../../images/tutorials/stretches/15.jpg'),
+    require('../../../images/tutorials/stretches/16.jpg'),
+    require('../../../images/tutorials/stretches/17.jpg')
 ]; 
 export const section3 = [
-    require('../../../images/tutorials/stretches/18.png'),
-    require('../../../images/tutorials/stretches/19.png'),
-    require('../../../images/tutorials/stretches/20.png'),
-    require('../../../images/tutorials/stretches/21.png'),
-    require('../../../images/tutorials/stretches/21.png'),
-    require('../../../images/tutorials/stretches/22.png'),
-    require('../../../images/tutorials/stretches/23.png'),
-    require('../../../images/tutorials/stretches/24.png')
+    require('../../../images/tutorials/stretches/18.jpg'),
+    require('../../../images/tutorials/stretches/19.jpg'),
+    require('../../../images/tutorials/stretches/20.jpg'),
+    require('../../../images/tutorials/stretches/21.jpg'),
+    require('../../../images/tutorials/stretches/21.jpg'),
+    require('../../../images/tutorials/stretches/22.jpg'),
+    require('../../../images/tutorials/stretches/23.jpg'),
+    require('../../../images/tutorials/stretches/24.jpg')
 ];
 export const section4 = [
-    require('../../../images/tutorials/stretches/25.png'),
-    require('../../../images/tutorials/stretches/26.png'),
-    require('../../../images/tutorials/stretches/26.png'),
-    require('../../../images/tutorials/stretches/27.png'),
-    require('../../../images/tutorials/stretches/28.png'),
-    require('../../../images/tutorials/stretches/29.png'),
-    require('../../../images/tutorials/stretches/30.png'),
-    require('../../../images/tutorials/stretches/31.png'),
-    require('../../../images/tutorials/stretches/32.png'),
-    require('../../../images/tutorials/stretches/33.png'),
-    require('../../../images/tutorials/stretches/34.png'),
-    require('../../../images/tutorials/stretches/35.png'),
-    require('../../../images/tutorials/stretches/36.png'),
-    require('../../../images/tutorials/stretches/37.png')
+    require('../../../images/tutorials/stretches/25.jpg'),
+    require('../../../images/tutorials/stretches/26.jpg'),
+    require('../../../images/tutorials/stretches/26.jpg'),
+    require('../../../images/tutorials/stretches/27.jpg'),
+    require('../../../images/tutorials/stretches/28.jpg'),
+    require('../../../images/tutorials/stretches/29.jpg'),
+    require('../../../images/tutorials/stretches/30.jpg'),
+    require('../../../images/tutorials/stretches/31.jpg'),
+    require('../../../images/tutorials/stretches/32.jpg'),
+    require('../../../images/tutorials/stretches/33.jpg'),
+    require('../../../images/tutorials/stretches/34.jpg'),
+    require('../../../images/tutorials/stretches/35.jpg'),
+    require('../../../images/tutorials/stretches/36.jpg'),
+    require('../../../images/tutorials/stretches/37.jpg')
 ];
 
 const sections = [{title: 'Leg Nerve Stretches', imgs: section1}, {title: 'Arm Nerve Stretches', imgs: section2}, {title:'Lower Body Stretches', imgs:section3}, {title:'Upper Body Stretches', imgs:section4}]
